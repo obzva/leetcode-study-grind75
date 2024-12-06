dcdb7725b4513df82872610154311787c51e5bb2
obzva <dngyng1000@gmail.com>
2024-12-06 17:12:53 +0900

    add solution: week1 merge two sorted lists

---
+++ week1/merge-two-sorted-lists.go
@@ -0,0 +1,36 @@
+/*
+Big O
+- N: the number of nodes of list1
+- M: the number of nodes of list2
+- Time complexity: O(N+M)
+- Space complexity: O(1)
+*/
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+ func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+	dummy := &ListNode{}
+	node := dummy
+	for list1 != nil && list2 != nil {
+		if list1.Val < list2.Val {
+			node.Next = list1
+			list1 = list1.Next
+		} else {
+			node.Next = list2
+			list2 = list2.Next
+		}
+		node = node.Next
+	}
+	if list1 != nil {
+		node.Next = list1
+	}
+	if list2 != nil {
+		node.Next = list2
+	}
+	return dummy.Next
+}
